015e368d78b755f0edaf87a2573a042bb98e77d0
5her10ck <43107757+5her10ck@users.noreply.github.com>
2019-11-05 05:50:39 -0500

    Update AppTest.java

--- src/test/java/com/viva/Calculator/AppTest.java
+++ src/test/java/com/viva/Calculator/AppTest.java
@@ -17,44 +17,53 @@ public class AppTest
 	
 	@Test
 	public void addTrue() {
-		assertEquals(25, app.add(12, 13));
+		assertEquals(55, app.add(42, 13));
 	}
 	@Test
 	public void addFail() {
-		assertNotEquals(25, app.add(15, 13));
+		assertNotEquals(32, app.add(15, 7));
+	
+	}
+	@Test
+	public void addTrue() {
+		assertNotEquals(0, app.add(0, 0));
 	
 	}
 	
 	@Test
 	public void subTrue() {
-		assertEquals(3, app.sub(16, 13));
+		assertEquals(12, app.sub(51, 39));
 	}
 	@Test
 	public void subFail() {
-		assertNotEquals(2, app.sub(16, 13));
+		assertNotEquals(5, app.sub(26, 17));
+	}
+	@Test
+	public void subFail() {
+		assertNotEquals(1, app.sub(12, 5));
 	}
 	
 	@Test
 	public void mulTrue() {
-		assertEquals(25, app.mul(5, 5));
+		assertEquals(45, app.mul(5, 9));
 	}
 	@Test
 	public void mulFail() {
-		assertNotEquals(25, app.mul(5, 6));
+		assertNotEquals(25, app.mul(3, 6));
 	}
 	
 	
 	@Test
 	public void divTrue() {
-		assertEquals(1, app.div(12, 12));
+		assertEquals(1, app.div(31, 31));
 	}
 	@Test
 	public void divFail() {
-		assertNotEquals(1, app.div(24, 12));
+		assertNotEquals(1, app.div(56, 7));
 	}
 	@Test(expected = ArithmeticException.class)
         public void testDivideByZero() {
-        int a = 15;
+        int a = 69;
         int b = 0;
         app.div(a, b);
     }
